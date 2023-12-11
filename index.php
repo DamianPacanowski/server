9134b635fa0f344fd0cabc6b2905c5510e04c430
pacanosiu <60273697+DamianPacanowski@users.noreply.github.com>
2023-12-11 12:01:32 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,60 +1,49 @@
 <?php
-	if(getenv("REMOTE_ADDR")=='::1')
+	for($x=0;$x<=9;$x++) 
 	{
-		for($x=0;$x<=9;$x++) 
+		for($y=0;$y<=9;$y++) 
 		{
-			for($y=0;$y<=9;$y++) 
+			for($z=0;$z<=9;$z++) 
 			{
-				for($z=0;$z<=9;$z++) 
-				{
-					$xyz_array[]=$x.$y.$z;
-				}
+				$xyz_array[]=$x.$y.$z;
 			}
 		}
-		if(isset($xyz_array))
-		{
-			foreach($xyz_array as $xyz)
-			{
-				$xyz_dir=implode('/',str_split($xyz));
-				if(is_dir($xyz_dir))
-				{
-					$server_dirs[]=$xyz_dir;
-				}
-			}
-		}
-		if(isset($server_dirs))
-		{
-			foreach($server_dirs as $server_dir)
-			{
-				$scandir_server_dir=scandir($server_dir);
-				foreach($scandir_server_dir as $server_files)
-				{
-					if((strlen($server_files)>2)&&(!is_dir($server_dir.'/'.$xyz_dir))&&(file_exists($server_dir.'/'.$server_files)))
-					{
-						$server_files_array[]=$server_dir.'/'.$server_files;
-					}
-				}
-			}
-		}
-		if(isset($server_files_array))
-		{
-			foreach($server_files_array as $server_files_dir)
-			{
-				if(file_exists($server_files_dir))
-				{
-					include_once($server_files_dir);
-				}
-			}
-		}
-		var_dump($server_files_array);
-		var_dump($server_dirs);
-		//var_dump($xyz_array);
 	}
-	else
+	if(isset($xyz_array))
 	{
-		echo 'serwer w modernizacji - wróć później';
+		foreach($xyz_array as $xyz)
+		{
+			$xyz_dir=implode('/',str_split($xyz));
+			if(is_dir($xyz_dir))
+			{
+				$server_dirs[]=$xyz_dir;
+			}
+		}
 	}
-	
+	if(isset($server_dirs))
+	{
+		foreach($server_dirs as $server_dir)
+		{
+			$scandir_server_dir=scandir($server_dir);
+			foreach($scandir_server_dir as $server_files)
+			{
+				if((strlen($server_files)>2)&&(!is_dir($server_dir.'/'.$xyz_dir))&&(file_exists($server_dir.'/'.$server_files)))
+				{
+					$server_files_array[]=$server_dir.'/'.$server_files;
+				}
+			}
+		}
+	}
+	if(isset($server_files_array))
+	{
+		foreach($server_files_array as $server_files_dir)
+		{
+			if(file_exists($server_files_dir))
+			{
+				include_once($server_files_dir);
+			}
+		}
+	}
 ?>
 
 
